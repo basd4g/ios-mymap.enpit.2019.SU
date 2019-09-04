544458267a644bd96914cb213a8eadaae5e5164e
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-04 15:28:02 +0900

    OK?

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
+    let manager = CLLocationManager()
+    @IBOutlet weak var mapVIew: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            manager.requestLocation()
+        }
+        label.text = "autorizedInUse"
+
+    }
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        print("locations : \(locations)")
+        let current = locations[0]
+        let region = MKCoordinateRegion.init(center: current.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapVIew.setRegion(region, animated: true)
+        label.text = "didUpdateLocations"
+
+    }
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error:Error) {
+        print("error = \(error)")
+        label.text = "didFailWithError"
     }
 
 
