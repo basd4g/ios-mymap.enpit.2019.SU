7d156c845d01e59ed3fb39c56f80f597e5fa7699
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-05 12:34:51 +0900

    fix extends class

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let manager = CLLocationManager()
     @IBOutlet weak var mapVIew: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.delegate = self
         if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
             manager.requestWhenInUseAuthorization()
         }
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         if status == .authorizedWhenInUse {
             manager.requestLocation()
         }
-        label.text = "autorizedInUse"
+//        print("autorizedInUse")
 
     }
     func locationManager(_ manager: CLLocationManager,
@@ -35,13 +35,13 @@ class ViewController: UIViewController {
         let current = locations[0]
         let region = MKCoordinateRegion.init(center: current.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
         mapVIew.setRegion(region, animated: true)
-        label.text = "didUpdateLocations"
+//        print("didUpdateLocations")
 
     }
     func locationManager(_ manager: CLLocationManager,
                          didFailWithError error:Error) {
         print("error = \(error)")
-        label.text = "didFailWithError"
+//        print("didFailWithError")
     }
 
 
